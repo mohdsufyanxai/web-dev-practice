937da3b0f7f1c6b25b85b955e5c3f58e66da3430
mohdsufyanxai <mohdsufyanxai@email.com>
2026-09-18 16:26:43 +0530

    add video 68

---
+++ video 68/script.js
@@ -0,0 +1,17 @@
+console.log("hatsunemikus")
+
+// let boxes = document.getElementsByClassName("box")
+
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "lightblue"
+
+// document.getElementById("red").style.backgroundColor = "lightblue";
+
+// document.querySelector(".box").style.backgroundColor = "lightblue";
+
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = ("red")
+})
