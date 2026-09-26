7aab34d04fda77b48f29edef56f971823b0e475e
mohdsufyanxai <mohdsufyanxai@email.com>
2026-09-26 15:17:52 +0530

    add video 82

---
+++ video 82/script.js
@@ -0,0 +1,34 @@
+async function sleep(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(45)
+        }, 1000);
+    })
+}
+
+
+function sun(a,b,c){
+    return a+b+c
+}
+
+
+(async function main() {
+//     let a = await sleep()
+//     console.log(a)
+//     let b = await sleep()
+//     console.log(b);
+// let [x,y, ...rest] = [1,2,3,4,5]
+// console.log(x,y,     rest)
+
+let obj = {
+    a:1,
+    b:2,
+    c:3
+}
+let {a, b} = obj
+console.log(a, b)
+
+let arr = (1, 3, 6)
+console.log(sum(arr[0],arr[1],arr[2]))
+
+})()
